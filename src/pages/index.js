075ad65fd49a8d76f24530b8035153be076cda06
Alex Preiss <alexspreiss@gmail.com>
2020-03-17 00:34:40 -0700

    SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO />
     <h1>Hi Sage</h1>
 
     <Link to="/page-2/">Go to page 2</Link>
